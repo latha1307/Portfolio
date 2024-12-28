85e4ef25bd120170a460da9d191787f14e316901
latha1307 <ldkannammal@gmail.com>
2024-12-28 18:56:02 +0530

    Initial commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,15 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import company from "./company.png"
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import image from './image.png'
 import image2 from './image2.png'
 import image3 from './image3.png'
@@ -61,12 +54,5 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
   company,
-  tripguide,
 };

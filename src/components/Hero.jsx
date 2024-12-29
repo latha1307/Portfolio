ce51189fcd6c16dc9c78eb6564ee4a29448a4287
latha1307 <ldkannammal@gmail.com>
2024-12-29 14:01:32 +0530

    minimized errors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-0 bottom-32 w-full flex justify-center items-center">
         <a href="#about" >
           <div className="w-[30px] h-[56px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">          
-            <motion.dev 
+            <motion.div 
               animate={{
                 y: [0, 24, 0]
               }}  
